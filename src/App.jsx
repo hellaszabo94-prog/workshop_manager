adee4100f000854b1df4ab2b2ab8d6ca1e2ec79e
Hella Szabo <szabo.hella94@gmail.com>
2026-09-13 19:43:18 +0200

    feat: add initial workshop manager page

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <main>
+      <h1>Workshop Manager</h1>
+
+      <p>Discover creative workshops and find your next experience.</p>
+    </main>
+  );
+}
+
+export default App;
